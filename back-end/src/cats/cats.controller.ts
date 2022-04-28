7859b8fc96e7023d0926b4ef0eeecf82617fb6c8
itsLuccas <felice.donaire@gmail.com>
2022-04-28 20:43:39 -0300

    database and cat_model

---
+++ back-end/src/cats/cats.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Res, HttpStatus, Body } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat } from './cat.entity';
+//import { CatDto } from './dto/create-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catService: CatsService) {}    
+
+  @Post()
+    async createCat(@Res() response, @Body() cat: Cat) {
+        const newCat = await this.catService.createCat(cat);
+        return response.status(HttpStatus.CREATED).json({
+            newCat
+        })
+    }  
+
+  @Get()
+  getHello(): Promise<Cat[]> {
+    return this.catService.findAll();
+  }
+}
---
+++ back-end/src/cats/cats.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Cat } from './cat.entity';
+
+
+@Injectable()
+export class CatsService {
+  constructor(
+    @Inject('CATS_REPOSITORY')
+    private readonly catsRepository: typeof Cat
+  ) {}
+
+  async findAll(): Promise<Cat[]> {
+    return this.catsRepository.findAll<Cat>();
+  }
+
+  async createCat(cat: any): Promise<Cat> {
+    return this.catsRepository.create<Cat>(cat);
+  }
+}
