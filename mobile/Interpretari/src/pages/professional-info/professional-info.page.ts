9dfcbd304cdaff1a1d01d61d2f752ad52cf64e7b
yHenrique <henriquey08@gmail.com>
2022-09-09 23:58:08 -0300

    Criação do modal de Info Pro

--- mobile/Interpretari/src/pages/professional-info/professional-info.page.ts
+++ mobile/Interpretari/src/pages/professional-info/professional-info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BooleanValueAccessor, ModalController, NavParams} from '@ionic/angular';
 
 @Component({
   selector: 'app-professional-info',
@@ -6,16 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professional-info.page.scss'],
 })
 export class ProfessionalInfoPage implements OnInit {
-
-  constructor() { }
+  
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
 
   isDisabled: boolean = true;
-  // isHidden: string = "hide";
   onEdit: string = "";
   offEdit: string = "ocultar";
+
+  proficiencia = [];
+  idioma_traducao: boolean = false;
+  idioma_interpretacao: boolean = false;
+  idiomaT = this.idioma_traducao? "ativo":"desativo";
+  idiomaI = this.idioma_interpretacao? "ativo":"desativo";
+
+  idioma_partida: string = "";
+  nativo_partida: boolean = false;
+  idioma_chegada: string = "";
+  nativo_chegada: boolean = false;
+  proficienciaList = [];
+
+  certificacaoList = [];
 
   changeBool() {
     if(this.isDisabled) {
@@ -26,5 +40,42 @@ export class ProfessionalInfoPage implements OnInit {
       this.offEdit = "ocultar";
     }
     this.isDisabled = !this.isDisabled;
+  }
+
+  addProficiencia() {
+    if (this.idioma_partida.length > 0) {
+      if (this.idioma_chegada.length > 0) {
+
+        if (this.idioma_traducao = true) {
+          this.proficiencia.push("ativo");
+        } else {
+          this.proficiencia.push("desativo");
+        }
+
+        if (this.idioma_interpretacao = true) {
+          this.proficiencia.push("ativo");
+        } else {
+          this.proficiencia.push("desativo");
+        }
+
+        let partida = this.idioma_partida;
+        this.proficiencia.push(partida);
+        // this.idioma_partida = "";
+
+        let chegada = this.idioma_chegada;
+        this.proficiencia.push(chegada);
+        // this.idioma_chegada = "";
+      }
+    }
+    this.proficienciaList.push(this.proficiencia);
+    this.idioma_traducao = false;
+    this.idioma_interpretacao = false;
+    this.modalCtrl.dismiss();
+    console.log(this.proficienciaList);
+    // this.proficienciaList.pop(this.proficiencia);
+  }
+
+  removeProficiencia(index) {
+    this.proficienciaList.splice(index, 1);
   }
 }
